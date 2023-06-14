5bef159fd78b94f93526549caaefbd060b73ded6
Yamikaze12 <59038986+Yamikaze12@users.noreply.github.com>
2023-06-14 12:53:41 +0700

    1

---
+++ Modules/PPDB/Resources/views/frontend/content/studi.blade.php
@@ -0,0 +1,46 @@
+   
+ <div class="container">
+        <h1 class="about-title">Jenjang Pendidikan</h1>
+        <p class="about-sub-title">Jenjang Pendidikan di Yayasan, Pilih Passion Kamu!</p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 wow fadeIn" data-wow-duration="2s" data-wow-delay=".1s">
+                <div class="service-box2">
+                    <div class="service-box-icon">
+                        <a href="#"><i class="fa fa-book" aria-hidden="true"></i></a>
+                    </div>
+                    <h3><a href="#">TKQ</a></h3>
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 wow fadeIn" data-wow-duration="2s" data-wow-delay=".4s">
+                <div class="service-box2">
+                    <div class="service-box-icon">
+                        <a href="#"><i class="fa fa-book" aria-hidden="true"></i></a>
+                    </div>
+                    <h3><a href="#">MI/SD</a></h3>
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 wow fadeIn" data-wow-duration="2s" data-wow-delay=".7s">
+                <div class="service-box2">
+                    <div class="service-box-icon">
+                        <a href="#"><i class="fa fa-book" aria-hidden="true"></i></a>
+                    </div>
+                    <h3><a href="#">MTS/SMP</a></h3>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 wow fadeIn" data-wow-duration="2s" data-wow-delay=".7s">
+                <div class="service-box2">
+                    <div class="service-box-icon">
+                        <a href="#"><i class="fa fa-book" aria-hidden="true"></i></a>
+                    </div>
+                    <h3><a href="#">MA/SMA</a></h3>
+                   
+                </div>
+            </div>
+            
+        </div>
+    </div>
